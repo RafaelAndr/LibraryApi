25ea4a04f7fa5e689ee6cfc4a9bebc10bc9b78b1
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-14 12:53:37 -0300

    feat(client): add encrypted password

--- src/main/java/rafaelandrade/libraryapi/service/ClientService.java
+++ src/main/java/rafaelandrade/libraryapi/service/ClientService.java
@@ -1,6 +1,7 @@
 package rafaelandrade.libraryapi.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import rafaelandrade.libraryapi.model.Client;
 import rafaelandrade.libraryapi.repository.ClientRepository;
@@ -10,8 +11,11 @@ import rafaelandrade.libraryapi.repository.ClientRepository;
 public class ClientService {
 
     private final ClientRepository repository;
+    private final PasswordEncoder encoder;
 
     public Client salvar(Client client){
+        var encryptedPassword = encoder.encode(client.getClientSecret());
+        client.setClientSecret(encryptedPassword);
         return repository.save(client);
     }
 
